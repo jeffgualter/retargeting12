96be9ee55eb2ec25c178aa72ccd8444d5622b1ed
jeffgualter <jeff.gualter@gmail.com>
2025-02-18 13:32:47 -0300

    Update kabum-360.js

--- public/scripts/kabum-360.js
+++ public/scripts/kabum-360.js
@@ -1,1 +1,1 @@
-<script>window.location.href = "https://www.awin1.com/cread.php?s=3836664&v=17729&q=491916&r=689591";</script>
+window.location.href = "https://www.awin1.com/cread.php?s=3836664&v=17729&q=491916&r=689591";
